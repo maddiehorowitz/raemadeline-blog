831f18dfe820be2e7c214e15a3edd9481e771be1
raemadeline <maddie.horowitz@addepar.com>
2017-01-16 14:20:53 -0500

    path names

--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/raemadeline/'
+    // ENV.rootURL = '/raemadeline/'
   }
 
   return ENV;
